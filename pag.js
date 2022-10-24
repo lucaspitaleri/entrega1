30aa9f0131269f91edfc981fc0251963751613d3
lucaspitaleri <lucaspitaleri2003@gmail.com>
2022-10-24 18:55:38 -0300

    first commit

---
+++ pag.js
@@ -0,0 +1,15 @@
+let cliente = prompt ("Bueno dias ingrese su nombre.");
+alert("bienvenido " + cliente)
+
+let zapatilla = prompt ("Quiere comprar zapatillas. Ponga si o no");
+if (zapatilla == "si"){
+    alert("Pefercto, ahora le vamos a mostrar los modelos que tenemos.")
+}
+
+else if (zapatilla == "no"){
+    alert("No vamos a poder ayudarlo en esta ocacion, mucha suerte!!")
+
+
+}
+
+alert("muchas gracias por su visita")
